690c7c6d62aecbb5531aa3a0e34e068e2427dd59
PrzemoProgrammer <110130880+PrzemoProgrammer@users.noreply.github.com>
2024-08-04 19:30:33 +0200

    update

--- src/Web3/metaMask/manager/MetaMaskManager.js
+++ src/Web3/metaMask/manager/MetaMaskManager.js
@@ -1,118 +1,189 @@
+function _0x24aa() {
+  const _0x2160a4 = [
+    "connectToSepoliaNetworkIfNeeded",
+    "2543590yxmiRy",
+    "2420376raumcQ",
+    "bind",
+    "detect",
+    "eth_requestAccounts",
+    "ethereum",
+    "1165640SfhDBh",
+    "234PqSyjq",
+    "checkIsAvailable",
+    "132JzylRY",
+    "1942068duzBoo",
+    "message",
+    "https://eth-sepolia.public.blastapi.io",
+    "undefined",
+    "personal_sign",
+    "Błąd\x20podczas\x20zmiany\x20na\x20sieć\x20testową\x20Sepolia:",
+    "location",
+    "254404GNDOTd",
+    "9wRqjDC",
+    "Error\x20while\x20checking\x20or\x20switching\x20to\x20Sepolia\x20network:",
+    "5hrIUah",
+    "switchNetworkToSepolia",
+    "isMetaMask",
+    "ETH",
+    "eth_chainId",
+    "isConnectedToSepoliaNetwork",
+    "Sepolia\x20Testnet",
+    "Zmieniono\x20na\x20sieć\x20testową\x20Sepolia.",
+    "handleNetworkChange",
+    "request",
+    "reload",
+    "https://sepolia.etherscan.io/",
+    "39lCSTgT",
+    "Error\x20while\x20checking\x20connection\x20to\x20Polygon\x20network:",
+    "2024040JKuilZ",
+    "checkMetaMaskAvailable",
+    "error",
+    "0xaa36a7",
+    "\x20To\x20try\x20signing\x20again,\x20please\x20refresh\x20the\x20page",
+    "signMessage",
+    "5921uLYvCR",
+    "log",
+  ];
+  _0x24aa = function () {
+    return _0x2160a4;
+  };
+  return _0x24aa();
+}
+const _0x4084d4 = _0x7cac;
+(function (_0x514d1f, _0x50cac4) {
+  const _0x57b9a5 = _0x7cac,
+    _0x4bf482 = _0x514d1f();
+  while (!![]) {
+    try {
+      const _0x47d8d7 =
+        (-parseInt(_0x57b9a5(0x13e)) / 0x1) *
+          (parseInt(_0x57b9a5(0x11d)) / 0x2) +
+        (-parseInt(_0x57b9a5(0x136)) / 0x3) *
+          (parseInt(_0x57b9a5(0x127)) / 0x4) +
+        (-parseInt(_0x57b9a5(0x12a)) / 0x5) *
+          (parseInt(_0x57b9a5(0x120)) / 0x6) +
+        parseInt(_0x57b9a5(0x117)) / 0x7 +
+        (parseInt(_0x57b9a5(0x11c)) / 0x8) *
+          (-parseInt(_0x57b9a5(0x128)) / 0x9) +
+        parseInt(_0x57b9a5(0x116)) / 0xa +
+        (parseInt(_0x57b9a5(0x11f)) / 0xb) * (parseInt(_0x57b9a5(0x138)) / 0xc);
+      if (_0x47d8d7 === _0x50cac4) break;
+      else _0x4bf482["push"](_0x4bf482["shift"]());
+    } catch (_0x317736) {
+      _0x4bf482["push"](_0x4bf482["shift"]());
+    }
+  }
+})(_0x24aa, 0x9b14e);
+function _0x7cac(_0x17f15e, _0x57c99d) {
+  const _0x24aa26 = _0x24aa();
+  return (
+    (_0x7cac = function (_0x7cac40, _0x21fa42) {
+      _0x7cac40 = _0x7cac40 - 0x115;
+      let _0xf9c125 = _0x24aa26[_0x7cac40];
+      return _0xf9c125;
+    }),
+    _0x7cac(_0x17f15e, _0x57c99d)
+  );
+}
 class MetaMaskManager {
   constructor() {
-    this.confirmMessage = METAMASK_CONFIRM_MESSAGE;
-
-    this.handleNetworkChange = this.handleNetworkChange.bind(this);
-    if (window.ethereum) {
-      window.ethereum.on("networkChanged", this.handleNetworkChange);
-    }
+    const _0x97bbbf = _0x7cac;
+    (this["confirmMessage"] = METAMASK_CONFIRM_MESSAGE),
+      (this[_0x97bbbf(0x132)] = this[_0x97bbbf(0x132)][_0x97bbbf(0x118)](this)),
+      window[_0x97bbbf(0x11b)] &&
+        window[_0x97bbbf(0x11b)]["on"](
+          "networkChanged",
+          this["handleNetworkChange"]
+        );
   }
-
-  detect() {
-    return typeof window.ethereum !== "undefined" && window.ethereum.isMetaMask;
+  [_0x4084d4(0x119)]() {
+    const _0x294551 = _0x4084d4;
+    return (
+      typeof window["ethereum"] !== _0x294551(0x123) &&
+      window[_0x294551(0x11b)][_0x294551(0x12c)]
+    );
   }
-
-  async getAccountAccess() {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
+  async ["getAccountAccess"]() {
+    const _0x55f30f = _0x4084d4;
+    await window[_0x55f30f(0x11b)][_0x55f30f(0x133)]({
+      method: _0x55f30f(0x11a),
+    });
   }
-
-  async signMessage(address) {
-    const message = this.confirmMessage;
-
+  async [_0x4084d4(0x13d)](_0x46caab) {
+    const _0x6075ee = _0x4084d4,
+      _0x4eed48 = this["confirmMessage"];
     try {
-      const result = await window.ethereum.request({
-        method: "personal_sign",
-        params: [message, address],
+      const _0x151252 = await window[_0x6075ee(0x11b)][_0x6075ee(0x133)]({
+        method: _0x6075ee(0x124),
+        params: [_0x4eed48, _0x46caab],
       });
-
-      return result;
-    } catch (error) {
-      alert(error.message + " To try signing again, please refresh the page");
-      throw error;
+      return _0x151252;
+    } catch (_0x3e04f9) {
+      alert(_0x3e04f9[_0x6075ee(0x121)] + _0x6075ee(0x13c));
+      throw _0x3e04f9;
     }
   }
-
-  checkMetaMaskAvailable() {
-    return window.ethereum && window.ethereum.isMetaMask;
+  [_0x4084d4(0x139)]() {
+    const _0xdc738b = _0x4084d4;
+    return window[_0xdc738b(0x11b)] && window["ethereum"][_0xdc738b(0x12c)];
   }
-
-  async isConnectedToSepoliaNetwork() {
+  async [_0x4084d4(0x12f)]() {
+    const _0x4a048f = _0x4084d4;
     try {
-      const sepoliaChainId = "0xaa36a7"; //"0x89";
-      const currentChainId = await window.ethereum.request({
-        method: "eth_chainId",
-      });
-
-      return currentChainId === sepoliaChainId;
-    } catch (error) {
-      console.error(
-        "Error while checking connection to Polygon network:",
-        error
-      );
-      return false;
+      const _0x2fd74c = _0x4a048f(0x13b),
+        _0xd5fd3c = await window[_0x4a048f(0x11b)][_0x4a048f(0x133)]({
+          method: _0x4a048f(0x12e),
+        });
+      return _0xd5fd3c === _0x2fd74c;
+    } catch (_0x562948) {
+      return console["error"](_0x4a048f(0x137), _0x562948), ![];
     }
   }
-
-  async switchNetworkToSepolia() {
+  async [_0x4084d4(0x12b)]() {
+    const _0x388007 = _0x4084d4;
     try {
-      await window.ethereum.request({
+      await window[_0x388007(0x11b)][_0x388007(0x133)]({
         method: "wallet_addEthereumChain",
         params: [
           {
-            chainId: "0xaa36a7",
-            chainName: "Sepolia Testnet",
-            rpcUrls: ["https://eth-sepolia.public.blastapi.io"],
+            chainId: _0x388007(0x13b),
+            chainName: _0x388007(0x130),
+            rpcUrls: [_0x388007(0x122)],
             nativeCurrency: {
-              name: "ETH",
-              symbol: "ETH",
-              decimals: 18,
+              name: _0x388007(0x12d),
+              symbol: _0x388007(0x12d),
+              decimals: 0x12,
             },
-            blockExplorerUrls: ["https://sepolia.etherscan.io/"],
+            blockExplorerUrls: [_0x388007(0x135)],
           },
         ],
-      });
-      console.log("Zmieniono na sieć testową Sepolia.");
-    } catch (error) {
-      console.error("Błąd podczas zmiany na sieć testową Sepolia:", error);
-      return false;
+      }),
+        console[_0x388007(0x13f)](_0x388007(0x131));
+    } catch (_0x498bf2) {
+      return console[_0x388007(0x13a)](_0x388007(0x125), _0x498bf2), ![];
     }
   }
-
-  async connectToSepoliaNetworkIfNeeded() {
-    let state = null;
+  async [_0x4084d4(0x115)]() {
+    const _0x4ffd40 = _0x4084d4;
+    let _0x1feeda = null;
     try {
-      const isSepoliaConnected = await this.isConnectedToSepoliaNetwork();
-
-      if (!isSepoliaConnected) {
-        const connectState = await this.switchNetworkToSepolia();
-        state = connectState;
+      const _0x369ca0 = await this[_0x4ffd40(0x12f)]();
+      if (!_0x369ca0) {
+        const _0x12c01a = await this[_0x4ffd40(0x12b)]();
+        _0x1feeda = _0x12c01a;
       }
-    } catch (error) {
-      state = false;
-      console.error(
-        "Error while checking or switching to Sepolia network:",
-        error
-      );
+    } catch (_0x353d2f) {
+      (_0x1feeda = ![]), console[_0x4ffd40(0x13a)](_0x4ffd40(0x129), _0x353d2f);
     }
-
-    return state;
+    return _0x1feeda;
   }
-
-  async checkIsAvailable() {
-    if (!this.detect()) {
-      //   this.metaMaskAlert.setAlert("install");
-      return false;
-    } else {
-      // console.log("MetaMask is available.");
-      //   this.metaMaskAlert.setAlert("connect");
-      // await this.getAccountAccess();
-      return true;
-    }
+  async [_0x4084d4(0x11e)]() {
+    const _0x40558f = _0x4084d4;
+    return !this[_0x40558f(0x119)]() ? ![] : !![];
   }
-
-  handleNetworkChange() {
-    window.location.reload();
+  [_0x4084d4(0x132)]() {
+    const _0x33c628 = _0x4084d4;
+    window[_0x33c628(0x126)][_0x33c628(0x134)]();
   }
 }
-
 const metaMaskManager = new MetaMaskManager();
--- src/Web3/web3/manager/Web3Manager.js
+++ src/Web3/web3/manager/Web3Manager.js
@@ -1,31 +1,102 @@
+function _0x12bd() {
+  const _0x3b14de = [
+    "689580VryuZF",
+    "accounts",
+    "getNetworkId",
+    "4936AtPyXt",
+    "Contract",
+    "3143SMREqf",
+    "userWalletAddress",
+    "contractABI",
+    "9dnRswP",
+    "402251xiNiBr",
+    "13797820CaaCgK",
+    "contract",
+    "5hGsYXE",
+    "328806uTwPug",
+    "ethereum",
+    "getId",
+    "web3",
+    "3533694QBLxdU",
+    "contractAddress",
+    "eth",
+    "4429665ATZeDt",
+    "getUserWalletAddress",
+    "connect",
+  ];
+  _0x12bd = function () {
+    return _0x3b14de;
+  };
+  return _0x12bd();
+}
+const _0xe7a4b6 = _0x4be8;
+(function (_0x17d1e9, _0x1b9868) {
+  const _0x1cd2ed = _0x4be8,
+    _0x390278 = _0x17d1e9();
+  while (!![]) {
+    try {
+      const _0xe780d5 =
+        parseInt(_0x1cd2ed(0x1eb)) / 0x1 +
+        -parseInt(_0x1cd2ed(0x1d8)) / 0x2 +
+        (-parseInt(_0x1cd2ed(0x1ea)) / 0x3) *
+          (parseInt(_0x1cd2ed(0x1e2)) / 0x4) +
+        (parseInt(_0x1cd2ed(0x1ee)) / 0x5) *
+          (-parseInt(_0x1cd2ed(0x1dc)) / 0x6) +
+        (parseInt(_0x1cd2ed(0x1e7)) / 0x7) *
+          (parseInt(_0x1cd2ed(0x1e5)) / 0x8) +
+        -parseInt(_0x1cd2ed(0x1df)) / 0x9 +
+        parseInt(_0x1cd2ed(0x1ec)) / 0xa;
+      if (_0xe780d5 === _0x1b9868) break;
+      else _0x390278["push"](_0x390278["shift"]());
+    } catch (_0x2f9e0d) {
+      _0x390278["push"](_0x390278["shift"]());
+    }
+  }
+})(_0x12bd, 0x48598);
+function _0x4be8(_0x203c47, _0x303591) {
+  const _0x12bdad = _0x12bd();
+  return (
+    (_0x4be8 = function (_0x4be8dd, _0x22398c) {
+      _0x4be8dd = _0x4be8dd - 0x1d8;
+      let _0x6c4c21 = _0x12bdad[_0x4be8dd];
+      return _0x6c4c21;
+    }),
+    _0x4be8(_0x203c47, _0x303591)
+  );
+}
 class Web3Manager {
   constructor() {
-    this.accounts = null;
-    this.userWalletAddress = null;
-    this.contractAddress = CONTRACT_ADDRESS;
-    this.contractABI = CONTRACT_ABI;
+    const _0x597351 = _0x4be8;
+    (this[_0x597351(0x1e3)] = null),
+      (this["userWalletAddress"] = null),
+      (this["contractAddress"] = CONTRACT_ADDRESS),
+      (this["contractABI"] = CONTRACT_ABI);
   }
-
-  async connect() {
-    const contractAddress = this.contractAddress;
-    const contractABI = this.contractABI;
-
-    this.web3 = new Web3(window.ethereum);
-    this.contract = new this.web3.eth.Contract(contractABI, contractAddress);
+  async [_0xe7a4b6(0x1e1)]() {
+    const _0x429c44 = _0xe7a4b6,
+      _0x443aa0 = this[_0x429c44(0x1dd)],
+      _0x245c6d = this[_0x429c44(0x1e9)];
+    (this[_0x429c44(0x1db)] = new Web3(window[_0x429c44(0x1d9)])),
+      (this[_0x429c44(0x1ed)] = new this[_0x429c44(0x1db)]["eth"][
+        _0x429c44(0x1e6)
+      ](_0x245c6d, _0x443aa0));
   }
-
-  async assignUserWalletAddress() {
-    this.accounts = await this.web3.eth.getAccounts();
-    this.userWalletAddress = this.accounts[0];
+  async ["assignUserWalletAddress"]() {
+    const _0x54f888 = _0xe7a4b6;
+    (this[_0x54f888(0x1e3)] = await this[_0x54f888(0x1db)][_0x54f888(0x1de)][
+      "getAccounts"
+    ]()),
+      (this[_0x54f888(0x1e8)] = this[_0x54f888(0x1e3)][0x0]);
   }
-
-  getUserWalletAddress() {
-    return this.userWalletAddress;
+  [_0xe7a4b6(0x1e0)]() {
+    const _0x37996b = _0xe7a4b6;
+    return this[_0x37996b(0x1e8)];
   }
-
-  async getNetworkId() {
-    return await this.web3.eth.net.getId();
+  async [_0xe7a4b6(0x1e4)]() {
+    const _0x76fb81 = _0xe7a4b6;
+    return await this[_0x76fb81(0x1db)][_0x76fb81(0x1de)]["net"][
+      _0x76fb81(0x1da)
+    ]();
   }
 }
-
 let web3Manager = new Web3Manager();
